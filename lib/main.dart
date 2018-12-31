311dfb197385158d1e44e971b3f83a53fe6a0cd2
Masaaki Kodama <tc003ssr@gmail.com>
2018-12-31 17:15:09 +0900

    complete Step 3.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // アイコンの下にラベルを持つwidgetを返す関数
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize:  12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    // タイトルセクションを定義
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32.0),
       child: Row(
@@ -39,5 +65,27 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+
+    // ボタンセクションを定義
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Building Layouts',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+    );
+
+
   }
 }
